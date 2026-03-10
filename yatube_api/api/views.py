ee854b43c0d9e6da8b703bed9cf63fb54c5acd3d
Vlad Zobnin <busytallk@yandex.ru>
2026-03-11 00:04:48 +0300

    add api/views

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,94 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, mixins, permissions, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import (
+    IsAuthenticated, IsAuthenticatedOrReadOnly
+)
+
+from posts.models import Group, Post
+from .permissions import IsAuthorOrReadOnly
+from .serializers import (
+    CommentSerializer,
+    FollowSerializer,
+    GroupSerializer,
+    PostSerializer
+)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для модели Post:
+    -GET /posts/ - список постов (пагинация)
+    -POST /posts/ - создание поста (только авторизованные)
+    -GET /posts/{id}/ - детали поста
+    -PUT/PATCH /posts/{id}/ - обновление поста (только автор)
+    -DELETE /posts/{id}/ - удаление поста (только автор)
+    """
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для модели Comment:
+    -GET /posts/{post_id}/comments/ - список комментариев
+    -POST /posts/{post_id}/comments/ - создание комментария
+    -GET /posts/{post_id}/comments/{id}/ - детали комментария
+    -PUT/PATCH /posts/{post_id}/comments/{id}/ - обновление (только автор)
+    -DELETE /posts/{post_id}/comments/{id} - удаление (только автор)
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    pagination_class = None
+
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs['post_id'])
+    
+    def get_queryset(self):
+        return self.get_post().comments.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post=self.get_post())
+        
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    ViewSet для модели Group (только чтение):
+    -GET /groups/ - список групп
+    -GET /groups/{id}/ - детали группы
+    """
+
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.AllowAny]
+    pagination_class = None
+
+
+class FollowViewSet(mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    """
+    ViewSet для модели Follow:
+    -GET /follow/ - список подписок текущего пользователя
+    -POST /follow/ - подписаться на пользователя
+    """
+
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['=following__username']
+    pagination_class = None
+
+    def get_queryset(self):
+        return self.request.user.follower.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
